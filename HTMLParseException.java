ee565d149923bc38432e5b4f4436f37e2d3d64a2
sam10sha <sam10sha@users.noreply.github.com>
2016-03-29 07:28:52 -0700

    Added files via upload

---
+++ HTMLParseException.java
@@ -0,0 +1,10 @@
+package big_boiz.antplanner;
+
+/**
+ * Created by Saumil on 2/29/2016.
+ */
+public class HTMLParseException extends RuntimeException {
+    public HTMLParseException(String error) {
+        super(error);
+    }
+}
